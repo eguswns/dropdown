729d70cc36670be500b611fe4b6f7f760558d9aa
eguswns <eguswnsee@gmail.com>
2025-08-29 04:29:57 +0900

    2nd

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,51 +1,24 @@
-import { useState } from "react";
 import { MENU_ITEMS } from "./menuData";
 import "./NavMenu.scss";
 
 export default function NavMenu() {
-  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
   return (
     <nav className="nav">
-      <div
-        className="nav__container"
-        onMouseLeave={() => setHoveredIndex(null)}
-      >
+      <div className="nav__container">
         <ul className="nav__menu">
           {MENU_ITEMS.map((menu, idx) => (
-            <li
-              key={idx}
-              className={`nav__item ${hoveredIndex === idx ? "active" : ""}`}
-              onMouseEnter={() => setHoveredIndex(idx)}
-            >
+            <li key={idx} className="nav__item">
               <div className="item__title">{menu.title}</div>
-            </li>
-          ))}
-        </ul>
-        {hoveredIndex !== null && (
-          <div className="subMenu">
-            {MENU_ITEMS.map((menu, idx) => (
-              <ul
-                key={idx}
-                className={`subMenu__list ${
-                  hoveredIndex === idx ? "active" : ""
-                }`}
-                onMouseEnter={() => setHoveredIndex(idx)}
-              >
+              <ul className="subMenu__list">
                 {menu.subTitles.map((subTitle, subIdx) => (
-                  <li
-                    key={subIdx}
-                    className={`subMenu__item ${
-                      hoveredIndex === idx ? "active" : ""
-                    }`}
-                  >
+                  <li key={subIdx} className="subMenu__item">
                     {subTitle}
                   </li>
                 ))}
               </ul>
-            ))}
-          </div>
-        )}
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
